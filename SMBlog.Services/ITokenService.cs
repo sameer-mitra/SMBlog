594ed1e969717804ebb50aec32e1d8612a7f4f63
Sameer Mitra <49610563+sameer-mitra@users.noreply.github.com>
2023-07-22 10:34:28 +1000

    SM 20230722 Added TokenService

--- SMBlog.Services/ITokenService.cs
+++ SMBlog.Services/ITokenService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMBlog.Models.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace SMBlog.Services
 {
     public interface ITokenService
     {
+        public string CreateToken(ApplicationUserIdentity user);
     }
 }
