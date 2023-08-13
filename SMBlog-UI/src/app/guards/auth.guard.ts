91def282df919a2145c860bebf7fd80ad114c702
Sameer Mitra <49610563+sameer-mitra@users.noreply.github.com>
2023-08-13 17:31:31 +1000

    SM 20230813 Minor fixes.

--- SMBlog-UI/src/app/guards/auth.guard.ts
+++ SMBlog-UI/src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AccountService } from '../services/account.service';
 
@@ -7,22 +7,22 @@ import { AccountService } from '../services/account.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-
+  
   constructor(
     private accountService: AccountService,
     private router: Router
-    ){}
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot):  boolean {
+    state: RouterStateSnapshot): boolean {
     
-    const currentUser = this.accountService.currentUserValue;
-    const isLoggedIn = currentUser && currentUser.token;
-    if(isLoggedIn){
-      return true;
-    }
-    this.router.navigate['/'];
-    return false;
+      if (this.accountService.isLoggedIn()) {
+        return true;
+      }
+    
+      this.router.navigate(['/']);
+      return false;
   }
   
 }
